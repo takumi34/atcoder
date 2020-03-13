dcd6c710d18b6b25774d65e032aaf71474b029e2
takumi34 <1shiranui@gmail.com>
2020-03-13 23:30:32 +0900

    add 001a.cpp

---
+++ abc001/001a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+
+typedef long long int ll;
+typedef pair<int, int> P;
+
+const long long INF = 3e18 + 12;
+const int inf = 1e9;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  cout << a - b << endl;
+
+  return 0;
+}
