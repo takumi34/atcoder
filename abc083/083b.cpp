1551e21773cf5bd3b5588f356a05d637ea02dd7e
takumi34 <1shiranui@gmail.com>
2020-03-17 05:29:49 +0900

    update 83b.cpp

--- abc083/083b.cpp
+++ abc083/083b.cpp
@@ -12,14 +12,8 @@ const long long INF = 3e18 + 12;
 const int inf = 1e9;
 
 int keta_sum(int a) {
-  string sta = to_string(a);
-  int keta = sta.length();
-  int sum = 0;
-  rep(i, keta) {
-    sum += sta[i] - '0';
-  }
-
-  return sum;
+  if (a < 10) return a;
+  return keta_sum(a / 10) + a % 10;
 }
 
 int main() {
