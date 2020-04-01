8052d6437228e8ebf8d3ee9d52206ea4ed4fcf88
takumi34 <1shiranui@gmail.com>
2020-04-02 04:00:53 +0900

    add 060b.cpp

---
+++ abc060/060b.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+typedef long long int ll;
+typedef pair<int, int> P;
+
+const long long INF = 3e18 + 12;
+const int inf = 1e9;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int a, b, c;
+  cin >> a >> b >> c;
+
+  rep(i, b) {
+    if (a * i % b == c) {
+      cout << "YES" << endl;
+      return 0;
+    }
+  }
+
+  cout << "NO" << endl;
+
+  return 0;
+}
