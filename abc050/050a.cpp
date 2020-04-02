5f4f74900732bfa88ae83b922db39346e40d972a
takumi34 <1shiranui@gmail.com>
2020-04-02 22:48:58 +0900

    add 050a 051a

---
+++ abc050/050a.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+typedef long long int ll;
+typedef pair<int, int> P;
+
+const long long INF = 3e18 + 12;
+const int inf = 1e9;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int a, b;
+  string p;
+  cin >> a >> p >> b;
+
+  if (p == "+") {
+    cout << a + b << endl;
+  } else {
+    cout << a - b << endl;
+  }
+
+  return 0;
+}
