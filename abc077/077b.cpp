0de7ff12e6714757beb19d47a34e2c8e008d568b
takumi34 <1shiranui@gmail.com>
2020-04-04 02:36:29 +0900

    add 077b.cpp

---
+++ abc077/077b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+typedef long long int ll;
+typedef pair<int, int> P;
+
+const long long INF = 3e18 + 12;
+const int inf = 1e9;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  ll n;
+  cin >> n;
+
+  ll a = sqrt(n);
+
+  cout << a * a << endl;
+
+  return 0;
+}
