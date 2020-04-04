a09a73306ffb5d26c4b9469af3dd1f856e7bd286
takumi34 <1shiranui@gmail.com>
2020-04-05 04:53:46 +0900

    add 094b.cpp

---
+++ abc094/094b.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define REP(i, n) for (int i = 1; i < (int)(n); i++)
+#define repi(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define uniqueV(x)          \
+  sort(x.begin(), x.end()); \
+  x.erase(unique(x.begin(), x.end()), x.end());
+#define debug(x) cerr << #x << ": " << x << '\n'
+#define debug2(x, y) cerr << "(" << #x << ", " << #y << ") = " \
+                          << "(" << x << ", " << y << ")" << '\n'
+
+typedef long long int ll;
+typedef pair<int, int> P;
+
+const long long INF = 3e18 + 12;
+const int inf = 1e9;
+
+// ----------------------------------------------
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n, m, x;
+  cin >> n >> m >> x;
+
+  vector<int> v(m);
+  rep(i, m) { cin >> v[i]; }
+
+  int a, b;
+  a = 0;
+  b = 0;
+
+  rep(i, m) {
+    if (v[i] < x) {
+      a++;
+    } else if (v[i] > x) {
+      b++;
+    }
+  }
+
+  cout << min(a, b) << endl;
+
+  return 0;
+}
