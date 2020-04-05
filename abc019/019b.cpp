94df237ddcde0448c0190cbf605022173406477a
takumi34 <1shiranui@gmail.com>
2020-04-06 05:18:47 +0900

    add 019b.cpp

---
+++ abc019/019b.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define REP(i, n) for (int i = 1; i <= (int)(n); i++)
+#define repi(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define uniqueV(x)          \
+  sort(x.begin(), x.end()); \
+  x.erase(unique(x.begin(), x.end()), x.end());
+#define debug(x) cerr << #x << ": " << x << '\n'
+#define debug2(x, y) cerr << "(" << #x << ", " << #y << ") = " \
+                          << "(" << x << ", " << y << ")" << '\n'
+
+typedef long long int ll;
+typedef pair<int, int> P;
+
+const long long INF = 3e18 + 12;
+const int inf = 1e9;
+
+// ----------------------------------------------
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  string s;
+  cin >> s;
+
+  int n = s.size();
+  int count = 1;
+
+  rep(i, n) {
+    if (s[i] == s[i + 1]) {
+      count++;
+    } else {
+      cout << s[i];
+      cout << count;
+      count = 1;
+    }
+  }
+
+  cout << endl;
+
+  return 0;
+}
