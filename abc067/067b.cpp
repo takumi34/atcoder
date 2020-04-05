65a0b3c0ed700e20e021ca0854caf448a7a1afc7
takumi34 <1shiranui@gmail.com>
2020-04-06 06:34:00 +0900

    add 067b.cpp

---
+++ abc067/067b.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define REP(i, n) for (int i = 1; i <= (int)(n); i++)
+#define repi(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define uniqueV(x)          \
+  sort(x.begin(), x.end()); \
+  x.erase(unique(x.begin(), x.end()), x.end());
+#define debug(x) cerr << #x << ": " << x << '\n'
+#define debug2(x, y) cerr << "(" << #x << ", " << #y << ") = " \
+                          << "(" << x << ", " << y << ")" << '\n'
+
+typedef long long int ll;
+typedef pair<int, int> P;
+
+const long long INF = 3e18 + 12;
+const int inf = 1e9;
+
+// ----------------------------------------------
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n, k;
+  cin >> n >> k;
+
+  vector<int> v(n);
+  rep(i, n) { cin >> v[i]; }
+
+  sort(all(v), greater<int>());
+  int sum = 0;
+  rep(i, k) {
+    sum += v[i];
+  }
+
+  cout << sum << endl;
+
+  return 0;
+}
