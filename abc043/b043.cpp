0aab5d23b0fde6fa99104b7115614e4b2081e3ca
takumi34 <1shiranui@gmail.com>
2020-06-07 02:28:47 +0900

    add b043

---
+++ abc043/b043.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define REP(i, n) for (int i = 1; i <= (int)(n); i++)
+#define repi(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define uniqueV(x)                                                             \
+  sort(x.begin(), x.end());                                                    \
+  x.erase(unique(x.begin(), x.end()), x.end());
+#define debug(x) cerr << #x << ": " << x << '\n'
+#define debug2(x, y)                                                           \
+  cerr << "(" << #x << ", " << #y << ") = "                                    \
+       << "(" << x << ", " << y << ")" << '\n'
+
+typedef long long int ll;
+typedef pair<int, int> P;
+
+const long long INF = 3e18 + 12;
+const int inf = 1e9;
+
+// ----------------------------------------------
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  string s;
+  cin >> s;
+
+  vector<int> v;
+
+  int ss = s.size();
+
+  rep(i, ss) {
+    if (s[i] == '0') {
+      v.push_back(0);
+    } else if (s[i] == '1') {
+      v.push_back(1);
+    } else {
+      if (v.size() == 0) { continue; }
+      v.pop_back();
+    }
+  }
+
+  for (int i : v) { cout << i; }
+  cout << endl;
+
+  return 0;
+}
