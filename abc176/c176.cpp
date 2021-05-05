802ea104fb0e2fe946ae26a714295a97414159f6
takumi34 <1shiranui@gmail.com>
2021-05-05 15:53:47 +0900

    add 176

---
+++ abc176/c176.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define REP(i, n) for (int i = 1; i <= (int)(n); i++)
+#define repi(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+#define uniqueV(x)                                                             \
+  sort(x.begin(), x.end());                                                    \
+  x.erase(unique(x.begin(), x.end()), x.end());
+#define debug(x) cerr << #x << ": " << x << '\n'
+#define debug2(x, y)                                                           \
+  cerr << "(" << #x << ", " << #y << ") = "                                    \
+       << "(" << x << ", " << y << ")" << '\n'
+
+typedef long long int ll;
+typedef pair<int, int> P;
+
+const long long INF = 3e18 + 12;
+const int inf = 1e9;
+
+// ----------------------------------------------
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+
+  vector<int> v(n);
+  rep(i, n) { cin >> v[i]; }
+
+  ll ans = 0;
+  int tmp = v[0];
+
+  rep(i, n - 1) {
+    tmp = max(tmp, v[i + 1]);
+    ans += tmp - v[i + 1];
+  }
+  cout << ans << endl;
+
+  return 0;
+}
